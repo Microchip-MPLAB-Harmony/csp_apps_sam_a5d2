710750e3248a2f3cbbc89e7dc75c57391b5afc55
Syam Nair <syam.nair@microchip.com>
2020-08-18 13:23:38 +0530

    [RTC] Regenerated demo for rtc_rtc_alarm_sam_a5d2_xult_iar
    MH3-41037
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/rtc/rtc_alarm/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
+++ apps/rtc/rtc_alarm/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
@@ -68,6 +68,9 @@ void PIO_Initialize ( void )
 	PIOB_REGS->PIO_MSKR = 0x1;
 	PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100;
 	
+ /* Port B Latch configuration */
+	PIOB_REGS->PIO_SODR = 0x1;
+	
 
 
  /* Port D Peripheral function A configuration */
