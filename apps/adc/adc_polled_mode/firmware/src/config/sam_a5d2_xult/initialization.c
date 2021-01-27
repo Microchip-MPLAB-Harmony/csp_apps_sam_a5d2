005dad2fbd28a2b00817f6fd36d698dc67c8d96f
Syam Nair <syam.nair@microchip.com>
2021-01-27 13:15:20 +0530

    [ADC] Regenerated demo for adc_adc_polled_mode_sam_a5d2_xult
    MH3-47518
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/adc/adc_polled_mode/firmware/src/config/sam_a5d2_xult/initialization.c
+++ apps/adc/adc_polled_mode/firmware/src/config/sam_a5d2_xult/initialization.c
@@ -124,6 +124,7 @@ static void STDIO_BufferModeSet(void)
 
 void SYS_Initialize ( void* data )
 {
+
     STDIO_BufferModeSet();
 
 
@@ -142,7 +143,7 @@ void SYS_Initialize ( void* data )
 
     PLIB_L2CC_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
