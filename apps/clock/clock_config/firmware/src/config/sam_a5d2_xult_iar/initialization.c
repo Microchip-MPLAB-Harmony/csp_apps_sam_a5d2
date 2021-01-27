a33292dc99c5f56f4ea7ce5599f6d86b1a820d63
Syam Nair <syam.nair@microchip.com>
2021-01-27 13:20:56 +0530

    [CLOCK] Regenerated demo for clock_clock_config_sam_a5d2_xult_iar
    MH3-47525
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/clock/clock_config/firmware/src/config/sam_a5d2_xult_iar/initialization.c
+++ apps/clock/clock_config/firmware/src/config/sam_a5d2_xult_iar/initialization.c
@@ -104,6 +104,7 @@
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -115,7 +116,7 @@ void SYS_Initialize ( void* data )
     MMU_Initialize();
     Matrix_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
