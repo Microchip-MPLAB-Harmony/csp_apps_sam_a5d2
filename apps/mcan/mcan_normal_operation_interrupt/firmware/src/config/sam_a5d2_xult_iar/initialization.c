8da0d3c003a856bfdf81d6716c73630f507e6e4f
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-02-05 13:20:34 +0530

    [MCAN] Regenerated demo for mcan_mcan_normal_operation_interrupt_sam_a5d2_xult_iar
    MH3-47550
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/mcan/mcan_normal_operation_interrupt/firmware/src/config/sam_a5d2_xult_iar/initialization.c
+++ apps/mcan/mcan_normal_operation_interrupt/firmware/src/config/sam_a5d2_xult_iar/initialization.c
@@ -106,6 +106,7 @@
 void SYS_Initialize ( void* data )
 {
 
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -119,7 +120,7 @@ void SYS_Initialize ( void* data )
 
     PLIB_L2CC_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
