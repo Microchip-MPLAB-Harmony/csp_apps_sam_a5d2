2de13ca8ce968d5ba71679a1e2571580bbe4dafd
Syam Nair <syam.nair@microchip.com>
2021-08-25 16:59:56 +0530

    [FLEXCOM] Regenerated demo for flexcom_usart_flexcom_usart_echo_interrupt_sam_a5d2_xult
    MH3-57900
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/flexcom/usart/flexcom_usart_echo_interrupt/firmware/src/config/sam_a5d2_xult/initialization.c
+++ apps/flexcom/usart/flexcom_usart_echo_interrupt/firmware/src/config/sam_a5d2_xult/initialization.c
@@ -106,12 +106,12 @@ void SYS_Initialize ( void* data )
 {
 
   
+    MMU_Initialize();
     CLK_Initialize();
 	PIO_Initialize();
 
 
 
-    MMU_Initialize();
     Matrix_Initialize();
 
     PLIB_L2CC_Initialize();
