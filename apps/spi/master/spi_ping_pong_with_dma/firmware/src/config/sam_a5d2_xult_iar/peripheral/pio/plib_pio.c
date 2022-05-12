e08e3a38113f90730f04f525103b94e644592162
Syam Nair <syam.nair@microchip.com>
2022-05-12 10:43:03 +0530

    [SPI] Regenerated demo for spi_master_spi_ping_pong_with_dma_sam_a5d2_xult_iar
    MH3-67088
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/spi/master/spi_ping_pong_with_dma/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
+++ apps/spi/master/spi_ping_pong_with_dma/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
@@ -44,6 +44,8 @@
 #include "plib_pio.h"
 #include "interrupts.h"
 
+
+
 /******************************************************************************
   Function:
     PIO_Initialize ( void )
@@ -60,11 +62,11 @@ void PIO_Initialize ( void )
  /* Port B Peripheral function GPIO configuration */
 	PIOB_REGS->PIO_MSKR = 0x20U;
 	PIOB_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port B Pin 5 configuration */
 	PIOB_REGS->PIO_MSKR = 0x20U;
 	PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-	
+
  /* Port B Latch configuration */
 	PIOB_REGS->PIO_SODR = 0x20U;
 
@@ -72,15 +74,15 @@ void PIO_Initialize ( void )
  /* Port D Peripheral function A configuration */
 	PIOD_REGS->PIO_MSKR = 0xe0003c0LU;
 	PIOD_REGS->PIO_CFGR = 0x1U;
-	
+
  /* Port D Peripheral function GPIO configuration */
 	PIOD_REGS->PIO_MSKR = 0x10000000LU;
 	PIOD_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port D Pin 28 configuration */
 	PIOD_REGS->PIO_MSKR = 0x10000000U;
 	PIOD_REGS->PIO_CFGR = (PIOD_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-	
+
  /* Port D Latch configuration */
 	PIOD_REGS->PIO_SODR = 0x10000000U;
 	PIOD_REGS->PIO_CODR = 0x10000000LU & ~0x10000000U;
@@ -124,6 +126,7 @@ uint32_t PIO_PortRead(PIO_PORT port)
     return PIO_REGS->PIO_GROUP[port].PIO_PDSR;
 }
 
+
 // *****************************************************************************
 /* Function:
     void PIO_PortWrite (PIO_PORT port, uint32_t mask, uint32_t value);
@@ -140,6 +143,7 @@ void PIO_PortWrite(PIO_PORT port, uint32_t mask, uint32_t value)
     PIO_REGS->PIO_GROUP[port].PIO_ODSR = value;
 }
 
+
 // *****************************************************************************
 /* Function:
     uint32_t PIO_PortLatchRead ( PIO_PORT port )
@@ -154,6 +158,7 @@ uint32_t PIO_PortLatchRead(PIO_PORT port)
 {
     return PIO_REGS->PIO_GROUP[port].PIO_ODSR;
 }
+
 
 // *****************************************************************************
 /* Function:
@@ -215,7 +220,7 @@ void PIO_PortToggle(PIO_PORT port, uint32_t mask)
 void PIO_PortInputEnable(PIO_PORT port, uint32_t mask)
 {
     PIO_REGS->PIO_GROUP[port].PIO_MSKR = mask;
-    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;	
+    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;
 }
 
 // *****************************************************************************
