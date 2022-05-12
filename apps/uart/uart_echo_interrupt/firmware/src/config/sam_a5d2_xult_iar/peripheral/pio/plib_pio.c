0236de8606b474963958602c12fb78705a0b4944
Syam Nair <syam.nair@microchip.com>
2022-05-12 11:17:05 +0530

    [UART] Regenerated demo for uart_uart_echo_interrupt_sam_a5d2_xult_iar
    MH3-67237
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/uart/uart_echo_interrupt/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
+++ apps/uart/uart_echo_interrupt/firmware/src/config/sam_a5d2_xult_iar/peripheral/pio/plib_pio.c
@@ -44,6 +44,8 @@
 #include "plib_pio.h"
 #include "interrupts.h"
 
+
+
 /******************************************************************************
   Function:
     PIO_Initialize ( void )
@@ -60,11 +62,11 @@ void PIO_Initialize ( void )
  /* Port B Peripheral function GPIO configuration */
 	PIOB_REGS->PIO_MSKR = 0x20U;
 	PIOB_REGS->PIO_CFGR = 0x0U;
-	
+
  /* Port B Pin 5 configuration */
 	PIOB_REGS->PIO_MSKR = 0x20U;
 	PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
-	
+
  /* Port B Latch configuration */
 	PIOB_REGS->PIO_SODR = 0x20U;
 
@@ -72,7 +74,7 @@ void PIO_Initialize ( void )
  /* Port D Peripheral function A configuration */
 	PIOD_REGS->PIO_MSKR = 0xcLU;
 	PIOD_REGS->PIO_CFGR = 0x1U;
-	
+
  /* Port D Latch configuration */
 	PIOD_REGS->PIO_CODR = 0x0LU;
 
@@ -115,6 +117,7 @@ uint32_t PIO_PortRead(PIO_PORT port)
     return PIO_REGS->PIO_GROUP[port].PIO_PDSR;
 }
 
+
 // *****************************************************************************
 /* Function:
     void PIO_PortWrite (PIO_PORT port, uint32_t mask, uint32_t value);
@@ -131,6 +134,7 @@ void PIO_PortWrite(PIO_PORT port, uint32_t mask, uint32_t value)
     PIO_REGS->PIO_GROUP[port].PIO_ODSR = value;
 }
 
+
 // *****************************************************************************
 /* Function:
     uint32_t PIO_PortLatchRead ( PIO_PORT port )
@@ -145,6 +149,7 @@ uint32_t PIO_PortLatchRead(PIO_PORT port)
 {
     return PIO_REGS->PIO_GROUP[port].PIO_ODSR;
 }
+
 
 // *****************************************************************************
 /* Function:
@@ -206,7 +211,7 @@ void PIO_PortToggle(PIO_PORT port, uint32_t mask)
 void PIO_PortInputEnable(PIO_PORT port, uint32_t mask)
 {
     PIO_REGS->PIO_GROUP[port].PIO_MSKR = mask;
-    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;	
+    PIO_REGS->PIO_GROUP[port].PIO_CFGR &= ~PIO_CFGR_DIR_Msk;
 }
 
 // *****************************************************************************
