27f4a1dd8152a5ae8e8742e1963badd4f76a78d2
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-04-21 14:06:38 +0530

    [UART] Regenerated demo for uart_uart_ring_buffer_interrupt_sam_a5d27_som1_ek
    MH3-78683

--- apps/uart/uart_ring_buffer_interrupt/firmware/src/config/sam_a5d27_som1_ek/interrupts.h
+++ apps/uart/uart_ring_buffer_interrupt/firmware/src/config/sam_a5d27_som1_ek/interrupts.h
@@ -57,6 +57,11 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void UART1_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
