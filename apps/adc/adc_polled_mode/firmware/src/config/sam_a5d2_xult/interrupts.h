6c55b4b6577099c16fe602f09d4f39ae3fe59f17
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-25 12:26:50 +0530

    [ADC] Regenerated demo for adc_adc_polled_mode_sam_a5d2_xult
    MH3-78479

--- apps/adc/adc_polled_mode/firmware/src/config/sam_a5d2_xult/interrupts.h
+++ apps/adc/adc_polled_mode/firmware/src/config/sam_a5d2_xult/interrupts.h
@@ -57,6 +57,11 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void PIT_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
