89db4b1575b450c363ed4537a6c90d5c0a1eff9f
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-25 12:27:36 +0530

    [SPI] Regenerated demo for spi_master_spi_self_loopback_interrupt_sam_a5d2_xult
    MH3-78663

--- apps/spi/master/spi_self_loopback_interrupt/firmware/src/config/sam_a5d2_xult/peripheral/spi/spi_master/plib_spi1_master.c
+++ apps/spi/master/spi_self_loopback_interrupt/firmware/src/config/sam_a5d2_xult/peripheral/spi/spi_master/plib_spi1_master.c
@@ -40,6 +40,17 @@
 *******************************************************************************/
 
 #include "plib_spi1_master.h"
+#include "interrupts.h"
+
+#define SPI_TDR_8BIT_REG      (*(volatile uint8_t* const)((SPI1_BASE_ADDRESS + SPI_TDR_REG_OFST)))
+
+#define SPI_TDR_9BIT_REG      (*(volatile uint16_t* const)((SPI1_BASE_ADDRESS + SPI_TDR_REG_OFST)))
+
+
+
+#define SPI_RDR_8BIT_REG      (*(volatile uint8_t* const)((SPI1_BASE_ADDRESS + SPI_RDR_REG_OFST)))
+
+#define SPI_RDR_9BIT_REG      (*(volatile uint16_t* const)((SPI1_BASE_ADDRESS + SPI_RDR_REG_OFST)))
 
 // *****************************************************************************
 // *****************************************************************************
@@ -48,7 +59,7 @@
 // *****************************************************************************
 
 /* Global object to save SPI Exchange related data */
-SPI_OBJECT spi1Obj;
+volatile static SPI_OBJECT spi1Obj;
 
 void SPI1_Initialize( void )
 {
@@ -60,7 +71,7 @@ void SPI1_Initialize( void )
     SPI1_REGS->SPI_MR = SPI_MR_MSTR_Msk | SPI_MR_DLYBCS(0) | SPI_MR_BRSRCCLK_PERIPH_CLK | SPI_MR_PCS_NPCS0  | SPI_MR_MODFDIS_Msk;
 
     /* Set up clock Polarity, data phase, Communication Width, Baud Rate */
-    SPI1_REGS->SPI_CSR[0] = SPI_CSR_CPOL_IDLE_LOW | SPI_CSR_NCPHA_VALID_LEADING_EDGE | SPI_CSR_BITS_8_BIT | SPI_CSR_SCBR(83)| SPI_CSR_DLYBS(0) | SPI_CSR_DLYBCT(0) | SPI_CSR_CSAAT_Msk;
+    SPI1_REGS->SPI_CSR[0] = SPI_CSR_CPOL_IDLE_LOW | SPI_CSR_NCPHA_VALID_LEADING_EDGE | SPI_CSR_BITS_8_BIT | SPI_CSR_SCBR(83)| SPI_CSR_DLYBS(0) | SPI_CSR_DLYBCT(0)  | SPI_CSR_CSAAT(1) ;
 
 
 
@@ -82,7 +93,7 @@ bool SPI1_WriteRead( void* pTransmitData, size_t txSize, void* pReceiveData, siz
     uint32_t dummyData;
 
     /* Verify the request */
-    if((((txSize > 0) && (pTransmitData != NULL)) || ((rxSize > 0) && (pReceiveData != NULL))) && (spi1Obj.transferIsBusy == false))
+    if((spi1Obj.transferIsBusy == false) && (((txSize > 0U) && (pTransmitData != NULL)) || ((rxSize > 0U) && (pReceiveData != NULL))))
     {
         isRequestAccepted = true;
         spi1Obj.txBuffer = pTransmitData;
@@ -115,23 +126,29 @@ bool SPI1_WriteRead( void* pTransmitData, size_t txSize, void* pReceiveData, siz
         dummyData = (SPI1_REGS->SPI_RDR & SPI_RDR_RD_Msk) >> SPI_RDR_RD_Pos;
         (void)dummyData;
 
-        if (spi1Obj.rxSize > spi1Obj.txSize)
+        size_t txSz = spi1Obj.txSize;
+
+        if (spi1Obj.rxSize > txSz)
         {
-            spi1Obj.dummySize = spi1Obj.rxSize - spi1Obj.txSize;
+            spi1Obj.dummySize = spi1Obj.rxSize - txSz;
         }
 
         /* Start the first write here itself, rest will happen in ISR context */
         if((SPI1_REGS->SPI_CSR[0] & SPI_CSR_BITS_Msk) == SPI_CSR_BITS_8_BIT)
         {
-            if (spi1Obj.txCount < spi1Obj.txSize)
+            if (spi1Obj.txCount < txSz)
             {
                 SPI1_REGS->SPI_TDR = *((uint8_t*)spi1Obj.txBuffer);
                 spi1Obj.txCount++;
             }
-            else if (spi1Obj.dummySize > 0)
+            else if (spi1Obj.dummySize > 0U)
             {
                 SPI1_REGS->SPI_TDR = (uint8_t)(0xff);
                 spi1Obj.dummySize--;
+            }
+            else
+            {
+                /* Do Nothing */
             }
         }
         else
@@ -140,19 +157,26 @@ bool SPI1_WriteRead( void* pTransmitData, size_t txSize, void* pReceiveData, siz
             spi1Obj.dummySize >>= 1;
             spi1Obj.rxSize >>= 1;
 
-            if (spi1Obj.txCount < spi1Obj.txSize)
+            txSz = spi1Obj.txSize;
+
+            if (spi1Obj.txCount < txSz)
             {
                 SPI1_REGS->SPI_TDR = *((uint16_t*)spi1Obj.txBuffer);
                 spi1Obj.txCount++;
             }
-            else if (spi1Obj.dummySize > 0)
+            else if (spi1Obj.dummySize > 0U)
             {
                 SPI1_REGS->SPI_TDR = (uint16_t)(0xffff);
                 spi1Obj.dummySize--;
             }
+            else
+            {
+                /* Do Nothing */
+
+            }
         }
 
-        if (rxSize > 0)
+        if (rxSize > 0U)
         {
             /* Enable receive interrupt to complete the transfer in ISR context */
             SPI1_REGS->SPI_IER = SPI_IER_RDRF_Msk;
@@ -182,12 +206,12 @@ bool SPI1_TransferSetup( SPI_TRANSFER_SETUP * setup, uint32_t spiSourceClock )
 {
     uint32_t scbr;
 
-    if ((setup == NULL) || (setup->clockFrequency == 0))
+    if ((setup == NULL) || (setup->clockFrequency == 0U))
     {
         return false;
     }
 
-    if(spiSourceClock == 0)
+    if(spiSourceClock == 0U)
     {
         // Fetch Master Clock Frequency directly
         spiSourceClock = 83000000;
@@ -195,13 +219,17 @@ bool SPI1_TransferSetup( SPI_TRANSFER_SETUP * setup, uint32_t spiSourceClock )
 
     scbr = spiSourceClock/setup->clockFrequency;
 
-    if(scbr == 0)
+    if(scbr == 0U)
     {
-        scbr = 1;
+        scbr = 1U;
     }
-    else if(scbr > 255)
+    else if(scbr > 255U)
     {
-        scbr = 255;
+        scbr = 255U;
+    }
+    else
+    {
+        /* Do Nothing */
     }
 
     SPI1_REGS->SPI_CSR[0] = (SPI1_REGS->SPI_CSR[0] & ~(SPI_CSR_CPOL_Msk | SPI_CSR_NCPHA_Msk | SPI_CSR_BITS_Msk | SPI_CSR_SCBR_Msk)) |((uint32_t)setup->clockPolarity | (uint32_t)setup->clockPhase | (uint32_t)setup->dataBits | SPI_CSR_SCBR(scbr));
@@ -211,7 +239,7 @@ bool SPI1_TransferSetup( SPI_TRANSFER_SETUP * setup, uint32_t spiSourceClock )
 
 bool SPI1_IsTransmitterBusy( void )
 {
-    return ((SPI1_REGS->SPI_SR & SPI_SR_TXEMPTY_Msk) == 0)? true : false;
+    return ((SPI1_REGS->SPI_SR & SPI_SR_TXEMPTY_Msk) == 0U)? true : false;
 }
 
 void SPI1_CallbackRegister( SPI_CALLBACK callback, uintptr_t context )
@@ -222,9 +250,11 @@ void SPI1_CallbackRegister( SPI_CALLBACK callback, uintptr_t context )
 
 bool SPI1_IsBusy( void )
 {
-    return ((spi1Obj.transferIsBusy) || ((SPI1_REGS->SPI_SR & SPI_SR_TXEMPTY_Msk) == 0));
+    bool transferIsBusy = spi1Obj.transferIsBusy;
+
+    return (((SPI1_REGS->SPI_SR & SPI_SR_TXEMPTY_Msk) == 0U) || (transferIsBusy));
 }
-void SPI1_InterruptHandler( void )
+void __attribute__((used)) SPI1_InterruptHandler( void )
 {
     uint32_t dataBits;
     uint32_t receivedData;
@@ -233,21 +263,27 @@ void SPI1_InterruptHandler( void )
     dataBits = SPI1_REGS->SPI_CSR[0] & SPI_CSR_BITS_Msk;
 
 
+    size_t rxCount = spi1Obj.rxCount;
+
     if ((SPI1_REGS->SPI_SR & SPI_SR_RDRF_Msk ) == SPI_SR_RDRF_Msk)
     {
         receivedData = (SPI1_REGS->SPI_RDR & SPI_RDR_RD_Msk) >> SPI_RDR_RD_Pos;
 
-        if (spi1Obj.rxCount < spi1Obj.rxSize)
+        if (rxCount < spi1Obj.rxSize)
         {
             if(dataBits == SPI_CSR_BITS_8_BIT)
             {
-                ((uint8_t*)spi1Obj.rxBuffer)[spi1Obj.rxCount++] = receivedData;
+                ((uint8_t*)spi1Obj.rxBuffer)[rxCount] =(uint8_t)receivedData;
+                rxCount++;
             }
             else
             {
-                ((uint16_t*)spi1Obj.rxBuffer)[spi1Obj.rxCount++] = receivedData;
+                ((uint16_t*)spi1Obj.rxBuffer)[rxCount] = (uint16_t)receivedData;
+                 rxCount++;
             }
         }
+
+        spi1Obj.rxCount = rxCount;
     }
 
     /* If there are more words to be transmitted, then transmit them here and keep track of the count */
@@ -257,31 +293,45 @@ void SPI1_InterruptHandler( void )
          * one byte is pending to be transmitted */
         SPI1_REGS->SPI_IDR = SPI_IDR_TDRE_Msk;
 
+        size_t txCount = spi1Obj.txCount;
+
         if(dataBits == SPI_CSR_BITS_8_BIT)
         {
-            if (spi1Obj.txCount < spi1Obj.txSize)
+            if (txCount < spi1Obj.txSize)
             {
-                SPI1_REGS->SPI_TDR = ((uint8_t*)spi1Obj.txBuffer)[spi1Obj.txCount++];
+                SPI1_REGS->SPI_TDR = ((uint8_t*)spi1Obj.txBuffer)[txCount];
+                txCount++;
             }
-            else if (spi1Obj.dummySize > 0)
+            else if (spi1Obj.dummySize > 0U)
             {
                 SPI1_REGS->SPI_TDR = (uint8_t)(0xff);
                 spi1Obj.dummySize--;
             }
+            else
+            {
+                /* Do Nothing */
+            }
         }
         else
         {
-            if (spi1Obj.txCount < spi1Obj.txSize)
+            if (txCount < spi1Obj.txSize)
             {
-                SPI1_REGS->SPI_TDR = ((uint16_t*)spi1Obj.txBuffer)[spi1Obj.txCount++];
+                SPI1_REGS->SPI_TDR = ((uint16_t*)spi1Obj.txBuffer)[txCount];
+                txCount++;
             }
-            else if (spi1Obj.dummySize > 0)
+            else if (spi1Obj.dummySize > 0U)
             {
                 SPI1_REGS->SPI_TDR = (uint16_t)(0xffff);
                 spi1Obj.dummySize--;
             }
+            else
+            {
+                /* Do Nothing */
+            }
         }
-        if ((spi1Obj.txCount == spi1Obj.txSize) && (spi1Obj.dummySize == 0))
+        size_t txSize = spi1Obj.txSize;
+
+        if ((spi1Obj.dummySize == 0U) && (txCount == txSize))
         {
             /* At higher baud rates, the data in the shift register can be
              * shifted out and TXEMPTY flag can get set resulting in a
@@ -295,7 +345,7 @@ void SPI1_InterruptHandler( void )
             isLastByteTransferInProgress = true;
 
         }
-        else if (spi1Obj.rxCount == spi1Obj.rxSize)
+        else if (rxCount == spi1Obj.rxSize)
         {
             /* Enable TDRE interrupt as all the requested bytes are received
              * and can now make use of the internal transmit shift register.
@@ -303,12 +353,18 @@ void SPI1_InterruptHandler( void )
             SPI1_REGS->SPI_IDR = SPI_IDR_RDRF_Msk;
             SPI1_REGS->SPI_IER = SPI_IDR_TDRE_Msk;
         }
+        else
+        {
+            /* Do Nothing */
+        }
+
+        spi1Obj.txCount = txCount;
     }
 
     /* See if Exchange is complete */
     if ((isLastByteTransferInProgress == true) && ((SPI1_REGS->SPI_SR & SPI_SR_TXEMPTY_Msk) == SPI_SR_TXEMPTY_Msk))
     {
-        if (spi1Obj.rxCount == spi1Obj.rxSize)
+        if (rxCount == spi1Obj.rxSize)
         {
             /* Set Last transfer to deassert NPCS after the last byte written in TDR has been transferred. */
             SPI1_REGS->SPI_CR = SPI_CR_LASTXFER_Msk;
@@ -322,7 +378,9 @@ void SPI1_InterruptHandler( void )
 
             if(spi1Obj.callback != NULL)
             {
-                spi1Obj.callback(spi1Obj.context);
+                uintptr_t context = spi1Obj.context;
+
+                spi1Obj.callback(context);
             }
         }
     }
