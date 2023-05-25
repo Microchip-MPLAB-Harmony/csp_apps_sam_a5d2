52affafa06156181f8e488172420bfff15c4e2cc
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-25 12:27:11 +0530

    [MCAN] Regenerated demo for mcan_mcan_fd_operation_interrupt_timestamp_sam_a5d2_xult
    MH3-78502

--- apps/mcan/mcan_fd_operation_interrupt_timestamp/firmware/src/config/sam_a5d2_xult/interrupts.h
+++ apps/mcan/mcan_fd_operation_interrupt_timestamp/firmware/src/config/sam_a5d2_xult/interrupts.h
@@ -57,6 +57,11 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void MCAN0_INT0_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
