0d6bb19f99ad1e39ca4e5ddbcea3e8bead5b5cbd
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-25 12:27:44 +0530

    [TWIHS] Regenerated demo for twihs_master_twihs_eeprom_sam_a5d2_xult
    MH3-78675

--- apps/twihs/master/twihs_eeprom/firmware/src/config/sam_a5d2_xult/interrupts.h
+++ apps/twihs/master/twihs_eeprom/firmware/src/config/sam_a5d2_xult/interrupts.h
@@ -57,6 +57,11 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void TWIHS1_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
--- apps/twihs/master/twihs_eeprom/firmware/src/config/sam_a5d2_xult/peripheral/pio/plib_pio.c
+++ apps/twihs/master/twihs_eeprom/firmware/src/config/sam_a5d2_xult/peripheral/pio/plib_pio.c
@@ -60,23 +60,23 @@ void PIO_Initialize ( void )
 {
 
  /* Port B Peripheral function GPIO configuration */
-	PIOB_REGS->PIO_MSKR = 0x20U;
-	PIOB_REGS->PIO_CFGR = 0x0U;
+   PIOB_REGS->PIO_MSKR = 0x20U;
+   PIOB_REGS->PIO_CFGR = 0x0U;
 
  /* Port B Pin 5 configuration */
-	PIOB_REGS->PIO_MSKR = 0x20U;
-	PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
+   PIOB_REGS->PIO_MSKR = 0x20U;
+   PIOB_REGS->PIO_CFGR = (PIOB_REGS->PIO_CFGR & (PIO_CFGR_FUNC_Msk)) | 0x100U;
 
  /* Port B Latch configuration */
-	PIOB_REGS->PIO_SODR = 0x20U;
+   PIOB_REGS->PIO_SODR = 0x20U;
 
 
  /* Port D Peripheral function A configuration */
-	PIOD_REGS->PIO_MSKR = 0x30LU;
-	PIOD_REGS->PIO_CFGR = 0x1U;
+   PIOD_REGS->PIO_MSKR = 0x30LU;
+   PIOD_REGS->PIO_CFGR = 0x1U;
 
  /* Port D Latch configuration */
-	PIOD_REGS->PIO_CODR = 0x0LU;
+   PIOD_REGS->PIO_CODR = 0x0LU;
 
 
 
