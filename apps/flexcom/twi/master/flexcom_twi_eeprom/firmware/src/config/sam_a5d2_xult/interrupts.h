469fdb3fd90b77261eb8c7f28273d85a72ccac36
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-25 12:27:03 +0530

    [FLEXCOM] Regenerated demo for flexcom_twi_master_flexcom_twi_eeprom_sam_a5d2_xult
    MH3-78493

--- apps/flexcom/twi/master/flexcom_twi_eeprom/firmware/src/config/sam_a5d2_xult/interrupts.h
+++ apps/flexcom/twi/master/flexcom_twi_eeprom/firmware/src/config/sam_a5d2_xult/interrupts.h
@@ -57,6 +57,11 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void FLEXCOM0_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
