b823059c2ed4be3a76aebf5b2df2d05d12ed7ec5
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2024-02-19 11:55:56 +0530

    [MCAN] Add mcan_mcan_normal_operation_blocking demo application for sam_a5d2_curiosity
    MH3-86897

---
+++ apps/mcan/mcan_fd_operation_blocking/firmware/src/main.c
@@ -0,0 +1,411 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+uint8_t Mcan0MessageRAM[MCAN0_MESSAGE_RAM_CONFIG_SIZE] __attribute__((aligned (32))) __attribute__((__section__(".region_nocache")));   
+
+/* Standard identifier id[28:18]*/
+#define WRITE_ID(id) (id << 18)
+#define READ_ID(id)  (id >> 18)
+
+static uint32_t status = 0;
+static uint8_t loop_count = 0;
+static uint8_t user_input = 0;
+
+static uint8_t txFiFo[MCAN0_TX_FIFO_BUFFER_SIZE];
+static uint8_t rxFiFo0[MCAN0_RX_FIFO0_SIZE];
+static uint8_t rxFiFo1[MCAN0_RX_FIFO1_SIZE];
+static uint8_t rxBuffer[MCAN0_RX_BUFFER_SIZE];
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* Message Length to Data length code */
+static uint8_t MCANLengthToDlcGet(uint8_t length)
+{
+    uint8_t dlc = 0;
+
+    if (length <= 8U)
+    {
+        dlc = length;
+    }
+    else if (length <= 12U)
+    {
+        dlc = 0x9U;
+    }
+    else if (length <= 16U)
+    {
+        dlc = 0xAU;
+    }
+    else if (length <= 20U)
+    {
+        dlc = 0xBU;
+    }
+    else if (length <= 24U)
+    {
+        dlc = 0xCU;
+    }
+    else if (length <= 32U)
+    {
+        dlc = 0xDU;
+    }
+    else if (length <= 48U)
+    {
+        dlc = 0xEU;
+    }
+    else
+    {
+        dlc = 0xFU;
+    }
+    return dlc;
+}
+
+/* Data length code to Message Length */
+static uint8_t MCANDlcToLengthGet(uint8_t dlc)
+{
+    uint8_t msgLength[] = {0U, 1U, 2U, 3U, 4U, 5U, 6U, 7U, 8U, 12U, 16U, 20U, 24U, 32U, 48U, 64U};
+    return msgLength[dlc];
+}
+
+/* Menu */
+static void display_menu(void)
+{
+	printf("Menu :\r\n"
+	       "  -- Select the action:\r\n"
+	       "  0: Send FD standard message with ID: 0x45A and 64 byte data 0 to 63. \r\n"
+	       "  1: Send FD standard message with ID: 0x469 and 64 byte data 128 to 191. \r\n"
+	       "  2: Send FD extended message with ID: 0x100000A5 and 64 byte data 0 to 63. \r\n"
+	       "  3: Send FD extended message with ID: 0x10000096 and 64 byte data 128 to 191. \r\n"
+	       "  4: Send normal standard message with ID: 0x469 and 8 byte data 0 to 7. \r\n"
+	       "  m: Display menu \r\n\r\n");
+}
+
+/* Print Rx Message */
+static void print_message(uint8_t numberOfMessage, MCAN_RX_BUFFER *rxBuf, uint8_t rxBufLen, uint8_t rxFifoBuf)
+{
+    uint8_t length = 0;
+    uint8_t msgLength = 0;
+    uint32_t id = 0;
+
+    if (rxFifoBuf == 0)
+        printf(" Rx FIFO0 :");
+    else if (rxFifoBuf == 1)
+        printf(" Rx FIFO1 :");
+    else if (rxFifoBuf == 2)
+        printf(" Rx Buffer :");
+
+    for (uint8_t count = 0; count < numberOfMessage; count++)
+    {
+        /* Print message to Console */
+        printf(" New Message Received\r\n");
+        id = rxBuf->xtd ? rxBuf->id : READ_ID(rxBuf->id);
+        msgLength = MCANDlcToLengthGet(rxBuf->dlc);
+        length = msgLength;
+        printf(" Message - ID : 0x%x Length : 0x%x ", (unsigned int)id, (unsigned int)msgLength);
+        printf("Message : ");
+        while(length)
+        {
+            printf("0x%x ", rxBuf->data[msgLength - length--]);
+        }
+        printf("\r\n");
+        rxBuf += rxBufLen;
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    MCAN_TX_BUFFER *txBuffer = NULL;
+    uint8_t        bufferNumber = 0;
+    uint8_t        numberOfMessage = 0;
+
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    printf(" ------------------------------ \r\n");
+    printf("            MCAN FD Demo          \r\n");
+    printf(" ------------------------------ \r\n");
+    
+    /* Set Message RAM Configuration */
+    MCAN0_MessageRAMConfigSet(Mcan0MessageRAM);
+
+    display_menu();
+     
+    while ( true )
+    {
+        /* Rx Buffers */
+        if (MCAN0_InterruptGet(MCAN_INTERRUPT_DRX_MASK))
+        {    
+            MCAN0_InterruptClear(MCAN_INTERRUPT_DRX_MASK);
+
+            /* Check MCAN Status */
+            status = MCAN0_ErrorGet();
+
+            if (((status & MCAN_PSR_LEC_Msk) == MCAN_ERROR_NONE) || ((status & MCAN_PSR_LEC_Msk) == MCAN_ERROR_LEC_NO_CHANGE))
+            {
+                if (MCAN0_RxBufferNumberGet(&bufferNumber))
+                {
+                    memset(rxBuffer, 0x00, MCAN0_RX_BUFFER_ELEMENT_SIZE);
+                    if (MCAN0_MessageReceive(bufferNumber, (MCAN_RX_BUFFER *)rxBuffer) == true)
+                    {
+                        print_message(1, (MCAN_RX_BUFFER *)rxBuffer, MCAN0_RX_BUFFER_ELEMENT_SIZE, 2);
+                    }
+                    else
+                    {
+                        printf(" Error in received message\r\n");
+                    }
+                }
+            }
+            else
+            {
+                printf(" Error in received message\r\n");
+            }
+        }
+
+        /* Rx FIFO0 */
+        if (MCAN0_InterruptGet(MCAN_INTERRUPT_RF0N_MASK))
+        {    
+            MCAN0_InterruptClear(MCAN_INTERRUPT_RF0N_MASK);
+
+            /* Check MCAN Status */
+            status = MCAN0_ErrorGet();
+
+            if (((status & MCAN_PSR_LEC_Msk) == MCAN_ERROR_NONE) || ((status & MCAN_PSR_LEC_Msk) == MCAN_ERROR_LEC_NO_CHANGE))
+            {
+                numberOfMessage = MCAN0_RxFifoFillLevelGet(MCAN_RX_FIFO_0);
+                if (numberOfMessage != 0)
+                {
+                    memset(rxFiFo0, 0x00, (numberOfMessage * MCAN0_RX_FIFO0_ELEMENT_SIZE));
+                    if (MCAN0_MessageReceiveFifo(MCAN_RX_FIFO_0, numberOfMessage, (MCAN_RX_BUFFER *)rxFiFo0) == true)
+                    {
+                        print_message(numberOfMessage, (MCAN_RX_BUFFER *)rxFiFo0, MCAN0_RX_FIFO0_ELEMENT_SIZE, 0);
+                    }
+                    else
+                    {
+                        printf(" Error in received message\r\n");
+                    }
+                }
+            }
+            else
+            {
+                printf(" Error in received message\r\n");
+            }
+        }
+
+        /* Rx FIFO1 */
+        if (MCAN0_InterruptGet(MCAN_INTERRUPT_RF1N_MASK))
+        {    
+            MCAN0_InterruptClear(MCAN_INTERRUPT_RF1N_MASK);
+
+            /* Check MCAN Status */
+            status = MCAN0_ErrorGet();
+
+            if (((status & MCAN_PSR_LEC_Msk) == MCAN_ERROR_NONE) || ((status & MCAN_PSR_LEC_Msk) == MCAN_ERROR_LEC_NO_CHANGE))
+            {
+                numberOfMessage = MCAN0_RxFifoFillLevelGet(MCAN_RX_FIFO_1);
+                if (numberOfMessage != 0)
+                {
+                    memset(rxFiFo1, 0x00, (numberOfMessage * MCAN0_RX_FIFO1_ELEMENT_SIZE));
+                    if (MCAN0_MessageReceiveFifo(MCAN_RX_FIFO_1, numberOfMessage, (MCAN_RX_BUFFER *)rxFiFo1) == true)
+                    {
+                        print_message(numberOfMessage, (MCAN_RX_BUFFER *)rxFiFo1, MCAN0_RX_FIFO1_ELEMENT_SIZE, 1);
+                    }
+                    else
+                    {
+                        printf(" Error in received message\r\n");
+                    }
+                }
+            }
+            else
+            {
+                printf(" Error in received message\r\n");
+            }
+        }
+
+        /* User input */
+        if (UART0_ReceiverIsReady() == false)
+        {
+            continue;
+        }
+        user_input = (uint8_t)UART0_ReadByte();
+
+        switch (user_input)
+        {
+            case '0':
+                memset(txFiFo, 0x00, MCAN0_TX_FIFO_BUFFER_ELEMENT_SIZE);
+                txBuffer = (MCAN_TX_BUFFER *)txFiFo;
+                txBuffer->id = WRITE_ID(0x45A);
+                txBuffer->dlc = MCANLengthToDlcGet(64);
+                txBuffer->fdf = 1;
+                txBuffer->brs = 1;
+                for (loop_count = 0; loop_count < 64; loop_count++){
+                    txBuffer->data[loop_count] = loop_count;
+                }                
+                printf("  0: Send FD standard message with ID: 0x45A and 64 byte data 0 to 63.\r\n");
+                if (MCAN0_MessageTransmitFifo(1, txBuffer) == true)
+                {    
+                    printf(" Success \r\n");
+                }
+                else
+                {
+                    printf(" Failed \r\n");
+                }             
+                break;  
+            case '1':
+                memset(txFiFo, 0x00, MCAN0_TX_FIFO_BUFFER_ELEMENT_SIZE);
+                txBuffer = (MCAN_TX_BUFFER *)txFiFo;
+                txBuffer->id = WRITE_ID(0x469);
+                txBuffer->dlc = MCANLengthToDlcGet(64);
+                txBuffer->fdf = 1;
+                txBuffer->brs = 1;
+                for (loop_count = 128; loop_count < 192; loop_count++){
+                    txBuffer->data[loop_count - 128] = loop_count;
+                }                
+                printf("  1: Send FD standard message with ID: 0x469 and 64 byte data 128 to 191.\r\n");
+                if (MCAN0_MessageTransmitFifo(1, txBuffer) == true)
+                {    
+                    printf(" Success \r\n");
+                }
+                else
+                {
+                    printf(" Failed \r\n");
+                }    
+                break;
+            case '2': 
+                memset(txFiFo, 0x00, MCAN0_TX_FIFO_BUFFER_ELEMENT_SIZE);
+                txBuffer = (MCAN_TX_BUFFER *)txFiFo;
+                txBuffer->id = 0x100000A5;
+                txBuffer->dlc = MCANLengthToDlcGet(64);
+                txBuffer->xtd = 1;
+                txBuffer->fdf = 1;
+                txBuffer->brs = 1;
+                for (loop_count = 0; loop_count < 64; loop_count++){
+                    txBuffer->data[loop_count] = loop_count;
+                }
+                printf("  2: Send FD extended message with ID: 0x100000A5 and 64 byte data 0 to 63.\r\n");
+                if (MCAN0_MessageTransmitFifo(1, txBuffer) == true)
+                {    
+                    printf(" Success \r\n");
+                }
+                else
+                {
+                    printf(" Failed \r\n");
+                }             
+                break;
+            case '3':
+                memset(txFiFo, 0x00, MCAN0_TX_FIFO_BUFFER_ELEMENT_SIZE);
+                txBuffer = (MCAN_TX_BUFFER *)txFiFo;
+                txBuffer->id = 0x10000096;
+                txBuffer->dlc = MCANLengthToDlcGet(64);
+                txBuffer->xtd = 1;
+                txBuffer->fdf = 1;
+                txBuffer->brs = 1;
+                for (loop_count = 128; loop_count < 192; loop_count++){
+                    txBuffer->data[loop_count - 128] = loop_count;
+                }
+                printf("  3: Send FD extended message with ID: 0x10000096 and 64 byte data 128 to 191.\r\n");
+                if (MCAN0_MessageTransmitFifo(1, txBuffer) == true)
+                {    
+                    printf(" Success \r\n");
+                }
+                else
+                {
+                    printf(" Failed \r\n");
+                }             
+                break;
+            
+            case '4':
+                memset(txFiFo, 0x00, MCAN0_TX_FIFO_BUFFER_ELEMENT_SIZE);
+                txBuffer = (MCAN_TX_BUFFER *)txFiFo;
+                txBuffer->id = WRITE_ID(0x469);
+                txBuffer->dlc = 8;
+                for (loop_count = 0; loop_count < 8; loop_count++){
+                    txBuffer->data[loop_count] = loop_count;
+                }                
+                printf("  4: Send normal standard message with ID: 0x469 and 8 byte data 0 to 7.\r\n");
+                if (MCAN0_MessageTransmitFifo(1, txBuffer) == true)
+                {    
+                    printf(" Success \r\n");
+                }
+                else
+                {
+                    printf(" Failed \r\n");
+                }             
+                break;                 
+
+            case 'm':
+            case 'M':
+                display_menu();
+                break;
+                
+            default:
+                printf(" Invalid Input \r\n");
+                break;
+        }  
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
---
+++ apps/mcan/mcan_fd_operation_blocking/readme.md
@@ -0,0 +1,97 @@
+---
+parent: Harmony 3 peripheral library application examples for SAMA5D2 family
+title: MCAN FD interrupt 
+has_children: false
+has_toc: false
+---
+
+[![MCHP](https://www.microchip.com/ResourcePackages/Microchip/assets/dist/images/logo.png)](https://www.microchip.com)
+
+# MCAN FD interrupt
+
+This example application shows how to use the MCAN module to transmit and receive CAN FD messages in polling mode.
+
+## Description
+
+This application transmits and receives CAN FD messages on the CAN bus. To run this application, two evaluation boards of same type are required. These boards acts as different nodes on the CAN bus. Same application is programmed onto both the boards. Boards are connected to PC via UART. While running the application, user can send and receive CAN FD messages between the boards using UART console applications running on the PC.
+
+## Downloading and building the application
+
+To clone or download this application from Github, go to the [main page of this repository](https://github.com/Microchip-MPLAB-Harmony/csp_apps_sam_a5d2) and then click Clone button to clone this repository or download as zip file.
+This content can also be downloaded using content manager by following these [instructions](https://github.com/Microchip-MPLAB-Harmony/contentmanager/wiki).
+
+Path of the application within the repository is **apps/mcan/mcan_fd_operation_blocking/firmware** .
+
+To build the application, refer to the following table and open the project using its IDE.
+
+| Project Name      | Description                                    |
+| ----------------- | ---------------------------------------------- |
+| sam_a5d2_curiosity.X | MPLABX project for [SAMA5D2 Curiosity]() |
+|||
+
+## Setting up AT91Bootstrap loader
+
+To load the application binary onto the target device, we need to use at91bootstrap loader. Refer to the [at91bootstrap loader documentation](../../docs/readme_bootstrap.md) for details on how to configure, build and run bootstrap loader project and use it to bootstrap the application binaries.
+
+## Setting up the hardware
+
+The following table shows the target hardware for the application projects.
+
+| Project Name| Board|
+|:---------|:---------:|
+| sam_a5d2_curiosity.X | [SAMA5D2 Curiosity]() |
+|||
+
+### Setting up [SAMA5D2 Curiosity]()
+
+#### Hardware required
+
+| Name |Quantity |
+| -------- | ------- |
+| [SAMA5D2 Curiosity]() | 2 |
+| [ATA6563 click Board](https://www.mikroe.com/ata6563-click) | 2 |
+|||
+
+
+#### Setting up the board
+
+- Connect [ATA6563 click Board](https://www.mikroe.com/ata6563-click) to [SAMA5D2 Xplained Ultra Evaluation Kits]() as per the Pin connections shown below
+
+    | [SAMA5D2 Xplained Ultra Evaluation Kits]() | [ATA6563 click Board](https://www.mikroe.com/ata6563-click) |
+    | ------ | ------- |
+    | CANTX0 (PIN4), mikroBUS1 Connector | RX (PIN 14) |
+    | CANRX0 (PIN6), mikroBUS1 Connector | TX (PIN 13) |
+    | 3V3 (PIN7),    mikroBUS1 Connector | 3V3 (PIN 7) |
+    | VDD_5V (PIN10), mikroBUS1 Connector| 5V (PIN 10) |
+    | GND (PIN8), mikroBUS1 Connector 	 | GND (PIN 9) |
+    |||
+
+- Connect [ATA6563 click Board](https://www.mikroe.com/ata6563-click) to each other using DB9 serial cable
+- Connect the Debug USB port on the boards to the computer using micro USB cables
+
+## Running the Application
+
+1. Build the application using its IDE
+2. Open the Terminal application (Ex.:Tera term) on the computer.
+3. Connect to the FTDI Virtual COM port and configure the serial settings as follows:
+    - Baud : 115200
+    - Data : 8 Bits
+    - Parity : None
+    - Stop : 1 Bit
+    - Flow Control : None
+4. In the console associated with board 1, press "0" to transmit a CAN FD message
+5. Transmitted message description and status will be displayed in the console window of board 1
+6. Received message will be displayed in the console window of board 2
+7. Follow the sequence below to send and receive different messages:
+    - Press '1' on board 1
+    - Press '2' on board 1
+    - Press '3' on board 1
+    - Press '4' on board 1
+    - Press '0' on board 2
+    - Press '1' on board 2
+    - Press '2' on board 2
+    - Press '3' on board 2
+    - Press '4' on board 2
+8. If the steps are executed in this sequence, the final output in the consoles will be as below (console on the left is the transmitter (board 1) and the one on the right is receiver (board 2)):
+
+![output](images/output_mcan_fd_operation_interrupt.png)
