ff158899157fa1c1ed2e4a64f18f1687dd54c8dc
Kevin Abishek S <kevinabishek.s@microchip.com>
2024-10-30 16:20:34 +0530

    [ADC] Regenerated demo for adc__adc_user_sequence__firmware__sama5d29_curiosity.X
    MH3-101230

--- apps/adc/adc_user_sequence/firmware/src/packs/ATSAMA5D29_DFP/component/sfrbu.h
+++ apps/adc/adc_user_sequence/firmware/src/packs/ATSAMA5D29_DFP/component/sfrbu.h
@@ -1,39 +1,29 @@
 /*
  * Component description for SFRBU
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
  *
- * Subject to your compliance with these terms, you may use Microchip software and any derivatives
- * exclusively with Microchip products. It is your responsibility to comply with third party license
- * terms applicable to your use of third party software (including open source software) that may
- * accompany Microchip software.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY,
- * APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND
- * FITNESS FOR A PARTICULAR PURPOSE.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
- * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
- * MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE FULLEST EXTENT
- * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT
- * EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
-/* file generated from device description version 2022-10-04T10:37:14Z */
-#ifndef _SAMA5D_SFRBU_COMPONENT_H_
-#define _SAMA5D_SFRBU_COMPONENT_H_
+/* file generated from device description file (ATDF) version 2024-06-24T14:43:55Z */
+#ifndef _SAMA5D2_SFRBU_COMPONENT_H_
+#define _SAMA5D2_SFRBU_COMPONENT_H_
 
 /* ************************************************************************** */
 /*   SOFTWARE API DEFINITION FOR SFRBU                                        */
 /* ************************************************************************** */
-
-/* -------- SFRBU_DDRBUMCR : (SFRBU Offset: 0x10) (R/W 32) DDR BU Mode Control Register -------- */
-#define SFRBU_DDRBUMCR_BUMEN_Pos              _UINT32_(0)                                          /* (SFRBU_DDRBUMCR) DDR BU Mode Enable Position */
-#define SFRBU_DDRBUMCR_BUMEN_Msk              (_UINT32_(0x1) << SFRBU_DDRBUMCR_BUMEN_Pos)          /* (SFRBU_DDRBUMCR) DDR BU Mode Enable Mask */
-#define SFRBU_DDRBUMCR_BUMEN(value)           (SFRBU_DDRBUMCR_BUMEN_Msk & (_UINT32_(value) << SFRBU_DDRBUMCR_BUMEN_Pos)) /* Assigment of value for BUMEN in the SFRBU_DDRBUMCR register */
-#define SFRBU_DDRBUMCR_Msk                    _UINT32_(0x00000001)                                 /* (SFRBU_DDRBUMCR) Register Mask  */
-
 
 /* -------- SFRBU_PSWBUCTRL : (SFRBU Offset: 0x00) (R/W 32) Power Switch BU Control Register -------- */
 #define SFRBU_PSWBUCTRL_SCTRL_Pos             _UINT32_(0)                                          /* (SFRBU_PSWBUCTRL) Power Switch BU Software Control Position */
@@ -54,13 +44,6 @@
 #define SFRBU_PSWBUCTRL_Msk                   _UINT32_(0xFFFFFF0F)                                 /* (SFRBU_PSWBUCTRL) Register Mask  */
 
 
-/* -------- SFRBU_RXLPPUCR : (SFRBU Offset: 0x14) (R/W 32) RXLP Pull-Up Control Register -------- */
-#define SFRBU_RXLPPUCR_RXDPUCTRL_Pos          _UINT32_(0)                                          /* (SFRBU_RXLPPUCR) RXLP RXD Pull-Up Control Position */
-#define SFRBU_RXLPPUCR_RXDPUCTRL_Msk          (_UINT32_(0x1) << SFRBU_RXLPPUCR_RXDPUCTRL_Pos)      /* (SFRBU_RXLPPUCR) RXLP RXD Pull-Up Control Mask */
-#define SFRBU_RXLPPUCR_RXDPUCTRL(value)       (SFRBU_RXLPPUCR_RXDPUCTRL_Msk & (_UINT32_(value) << SFRBU_RXLPPUCR_RXDPUCTRL_Pos)) /* Assigment of value for RXDPUCTRL in the SFRBU_RXLPPUCR register */
-#define SFRBU_RXLPPUCR_Msk                    _UINT32_(0x00000001)                                 /* (SFRBU_RXLPPUCR) Register Mask  */
-
-
 /* -------- SFRBU_TSRANGECFG : (SFRBU Offset: 0x04) (R/W 32) TS Range Configuration Register -------- */
 #define SFRBU_TSRANGECFG_TSHRSEL_Pos          _UINT32_(0)                                          /* (SFRBU_TSRANGECFG) Temperature Sensor Range Selection Position */
 #define SFRBU_TSRANGECFG_TSHRSEL_Msk          (_UINT32_(0x1) << SFRBU_TSRANGECFG_TSHRSEL_Pos)      /* (SFRBU_TSRANGECFG) Temperature Sensor Range Selection Mask */
@@ -68,11 +51,25 @@
 #define SFRBU_TSRANGECFG_Msk                  _UINT32_(0x00000001)                                 /* (SFRBU_TSRANGECFG) Register Mask  */
 
 
+/* -------- SFRBU_DDRBUMCR : (SFRBU Offset: 0x10) (R/W 32) DDR BU Mode Control Register -------- */
+#define SFRBU_DDRBUMCR_BUMEN_Pos              _UINT32_(0)                                          /* (SFRBU_DDRBUMCR) DDR BU Mode Enable Position */
+#define SFRBU_DDRBUMCR_BUMEN_Msk              (_UINT32_(0x1) << SFRBU_DDRBUMCR_BUMEN_Pos)          /* (SFRBU_DDRBUMCR) DDR BU Mode Enable Mask */
+#define SFRBU_DDRBUMCR_BUMEN(value)           (SFRBU_DDRBUMCR_BUMEN_Msk & (_UINT32_(value) << SFRBU_DDRBUMCR_BUMEN_Pos)) /* Assigment of value for BUMEN in the SFRBU_DDRBUMCR register */
+#define SFRBU_DDRBUMCR_Msk                    _UINT32_(0x00000001)                                 /* (SFRBU_DDRBUMCR) Register Mask  */
+
+
+/* -------- SFRBU_RXLPPUCR : (SFRBU Offset: 0x14) (R/W 32) RXLP Pull-Up Control Register -------- */
+#define SFRBU_RXLPPUCR_RXDPUCTRL_Pos          _UINT32_(0)                                          /* (SFRBU_RXLPPUCR) RXLP RXD Pull-Up Control Position */
+#define SFRBU_RXLPPUCR_RXDPUCTRL_Msk          (_UINT32_(0x1) << SFRBU_RXLPPUCR_RXDPUCTRL_Pos)      /* (SFRBU_RXLPPUCR) RXLP RXD Pull-Up Control Mask */
+#define SFRBU_RXLPPUCR_RXDPUCTRL(value)       (SFRBU_RXLPPUCR_RXDPUCTRL_Msk & (_UINT32_(value) << SFRBU_RXLPPUCR_RXDPUCTRL_Pos)) /* Assigment of value for RXDPUCTRL in the SFRBU_RXLPPUCR register */
+#define SFRBU_RXLPPUCR_Msk                    _UINT32_(0x00000001)                                 /* (SFRBU_RXLPPUCR) Register Mask  */
+
+
 /** \brief SFRBU register offsets definitions */
-#define SFRBU_DDRBUMCR_REG_OFST        _UINT32_(0x10)      /* (SFRBU_DDRBUMCR) DDR BU Mode Control Register Offset */
 #define SFRBU_PSWBUCTRL_REG_OFST       _UINT32_(0x00)      /* (SFRBU_PSWBUCTRL) Power Switch BU Control Register Offset */
-#define SFRBU_RXLPPUCR_REG_OFST        _UINT32_(0x14)      /* (SFRBU_RXLPPUCR) RXLP Pull-Up Control Register Offset */
 #define SFRBU_TSRANGECFG_REG_OFST      _UINT32_(0x04)      /* (SFRBU_TSRANGECFG) TS Range Configuration Register Offset */
+#define SFRBU_DDRBUMCR_REG_OFST        _UINT32_(0x10)      /* (SFRBU_DDRBUMCR) DDR BU Mode Control Register Offset */
+#define SFRBU_RXLPPUCR_REG_OFST        _UINT32_(0x14)      /* (SFRBU_RXLPPUCR) RXLP Pull-Up Control Register Offset */
 
 #if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
 /** \brief SFRBU register API structure */
@@ -87,4 +84,4 @@ typedef struct
 
 
 #endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
-#endif /* _SAMA5D_SFRBU_COMPONENT_H_ */
+#endif /* _SAMA5D2_SFRBU_COMPONENT_H_ */
