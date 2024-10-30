9703c47159924b1ca68a6baae4431f06be486ff0
Kevin Abishek S <kevinabishek.s@microchip.com>
2024-10-30 16:20:51 +0530

    [SPI] Regenerated demo for spi__master__spi_ping_pong_with_dma__firmware__sama5d29_curiosity.X
    MH3-101284

--- apps/spi/master/spi_ping_pong_with_dma/firmware/src/packs/ATSAMA5D29_DFP/component/pit.h
+++ apps/spi/master/spi_ping_pong_with_dma/firmware/src/packs/ATSAMA5D29_DFP/component/pit.h
@@ -1,28 +1,25 @@
 /*
  * Component description for PIT
  *
- * Copyright (c) 2022 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
  *
- * Subject to your compliance with these terms, you may use Microchip software and any derivatives
- * exclusively with Microchip products. It is your responsibility to comply with third party license
- * terms applicable to your use of third party software (including open source software) that may
- * accompany Microchip software.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY,
- * APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND
- * FITNESS FOR A PARTICULAR PURPOSE.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
- * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF
- * MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE FULLEST EXTENT
- * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT
- * EXCEED THE AMOUNT OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
-/* file generated from device description version 2022-10-04T10:37:14Z */
-#ifndef _SAMA5D_PIT_COMPONENT_H_
-#define _SAMA5D_PIT_COMPONENT_H_
+/* file generated from device description file (ATDF) version 2024-06-24T14:43:55Z */
+#ifndef _SAMA5D2_PIT_COMPONENT_H_
+#define _SAMA5D2_PIT_COMPONENT_H_
 
 /* ************************************************************************** */
 /*   SOFTWARE API DEFINITION FOR PIT                                          */
@@ -41,14 +38,11 @@
 #define PIT_MR_Msk                            _UINT32_(0x030FFFFF)                                 /* (PIT_MR) Register Mask  */
 
 
-/* -------- PIT_PIIR : (PIT Offset: 0x0C) ( R/ 32) Periodic Interval Image Register -------- */
-#define PIT_PIIR_CPIV_Pos                     _UINT32_(0)                                          /* (PIT_PIIR) Current Periodic Interval Value Position */
-#define PIT_PIIR_CPIV_Msk                     (_UINT32_(0xFFFFF) << PIT_PIIR_CPIV_Pos)             /* (PIT_PIIR) Current Periodic Interval Value Mask */
-#define PIT_PIIR_CPIV(value)                  (PIT_PIIR_CPIV_Msk & (_UINT32_(value) << PIT_PIIR_CPIV_Pos)) /* Assigment of value for CPIV in the PIT_PIIR register */
-#define PIT_PIIR_PICNT_Pos                    _UINT32_(20)                                         /* (PIT_PIIR) Periodic Interval Counter Position */
-#define PIT_PIIR_PICNT_Msk                    (_UINT32_(0xFFF) << PIT_PIIR_PICNT_Pos)              /* (PIT_PIIR) Periodic Interval Counter Mask */
-#define PIT_PIIR_PICNT(value)                 (PIT_PIIR_PICNT_Msk & (_UINT32_(value) << PIT_PIIR_PICNT_Pos)) /* Assigment of value for PICNT in the PIT_PIIR register */
-#define PIT_PIIR_Msk                          _UINT32_(0xFFFFFFFF)                                 /* (PIT_PIIR) Register Mask  */
+/* -------- PIT_SR : (PIT Offset: 0x04) ( R/ 32) Status Register -------- */
+#define PIT_SR_PITS_Pos                       _UINT32_(0)                                          /* (PIT_SR) Periodic Interval Timer Status Position */
+#define PIT_SR_PITS_Msk                       (_UINT32_(0x1) << PIT_SR_PITS_Pos)                   /* (PIT_SR) Periodic Interval Timer Status Mask */
+#define PIT_SR_PITS(value)                    (PIT_SR_PITS_Msk & (_UINT32_(value) << PIT_SR_PITS_Pos)) /* Assigment of value for PITS in the PIT_SR register */
+#define PIT_SR_Msk                            _UINT32_(0x00000001)                                 /* (PIT_SR) Register Mask  */
 
 
 /* -------- PIT_PIVR : (PIT Offset: 0x08) ( R/ 32) Periodic Interval Value Register -------- */
@@ -61,18 +55,21 @@
 #define PIT_PIVR_Msk                          _UINT32_(0xFFFFFFFF)                                 /* (PIT_PIVR) Register Mask  */
 
 
-/* -------- PIT_SR : (PIT Offset: 0x04) ( R/ 32) Status Register -------- */
-#define PIT_SR_PITS_Pos                       _UINT32_(0)                                          /* (PIT_SR) Periodic Interval Timer Status Position */
-#define PIT_SR_PITS_Msk                       (_UINT32_(0x1) << PIT_SR_PITS_Pos)                   /* (PIT_SR) Periodic Interval Timer Status Mask */
-#define PIT_SR_PITS(value)                    (PIT_SR_PITS_Msk & (_UINT32_(value) << PIT_SR_PITS_Pos)) /* Assigment of value for PITS in the PIT_SR register */
-#define PIT_SR_Msk                            _UINT32_(0x00000001)                                 /* (PIT_SR) Register Mask  */
+/* -------- PIT_PIIR : (PIT Offset: 0x0C) ( R/ 32) Periodic Interval Image Register -------- */
+#define PIT_PIIR_CPIV_Pos                     _UINT32_(0)                                          /* (PIT_PIIR) Current Periodic Interval Value Position */
+#define PIT_PIIR_CPIV_Msk                     (_UINT32_(0xFFFFF) << PIT_PIIR_CPIV_Pos)             /* (PIT_PIIR) Current Periodic Interval Value Mask */
+#define PIT_PIIR_CPIV(value)                  (PIT_PIIR_CPIV_Msk & (_UINT32_(value) << PIT_PIIR_CPIV_Pos)) /* Assigment of value for CPIV in the PIT_PIIR register */
+#define PIT_PIIR_PICNT_Pos                    _UINT32_(20)                                         /* (PIT_PIIR) Periodic Interval Counter Position */
+#define PIT_PIIR_PICNT_Msk                    (_UINT32_(0xFFF) << PIT_PIIR_PICNT_Pos)              /* (PIT_PIIR) Periodic Interval Counter Mask */
+#define PIT_PIIR_PICNT(value)                 (PIT_PIIR_PICNT_Msk & (_UINT32_(value) << PIT_PIIR_PICNT_Pos)) /* Assigment of value for PICNT in the PIT_PIIR register */
+#define PIT_PIIR_Msk                          _UINT32_(0xFFFFFFFF)                                 /* (PIT_PIIR) Register Mask  */
 
 
 /** \brief PIT register offsets definitions */
 #define PIT_MR_REG_OFST                _UINT32_(0x00)      /* (PIT_MR) Mode Register Offset */
-#define PIT_PIIR_REG_OFST              _UINT32_(0x0C)      /* (PIT_PIIR) Periodic Interval Image Register Offset */
-#define PIT_PIVR_REG_OFST              _UINT32_(0x08)      /* (PIT_PIVR) Periodic Interval Value Register Offset */
 #define PIT_SR_REG_OFST                _UINT32_(0x04)      /* (PIT_SR) Status Register Offset */
+#define PIT_PIVR_REG_OFST              _UINT32_(0x08)      /* (PIT_PIVR) Periodic Interval Value Register Offset */
+#define PIT_PIIR_REG_OFST              _UINT32_(0x0C)      /* (PIT_PIIR) Periodic Interval Image Register Offset */
 
 #if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
 /** \brief PIT register API structure */
@@ -86,4 +83,4 @@ typedef struct
 
 
 #endif /* !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__)) */
-#endif /* _SAMA5D_PIT_COMPONENT_H_ */
+#endif /* _SAMA5D2_PIT_COMPONENT_H_ */
